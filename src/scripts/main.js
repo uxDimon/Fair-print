ba1d80dc711e1c5c7dadbc24e1a1bf7e2b2c0f9f
Kate <catherina1punk@gmail.com>
2020-12-08 19:06:55 +0300

    + main slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,13 @@
 let text = "Привет Васян";
 console.log(text);
+
+var mySwiper = new Swiper('.main-top__container', {
+    // Optional parameters
+    loop: true,
+    spaceBetween: 30,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+});
