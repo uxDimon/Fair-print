f1edb2ad4d52ff9ad02286cb9ac00e92d072d4a1
Kate <catherina1punk@gmail.com>
2020-12-09 19:11:25 +0300

    + aloums main

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,3 +11,33 @@ var mySwiper = new Swiper('.main-top__container', {
       el: '.swiper-pagination',
     },
 });
+var mySwiper = new Swiper('.clients__swiper-top', {
+  // Optional parameters
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
+  slidesPerView: 6,
+  spaceBetween: 30,
+});
+var mySwiper = new Swiper('.clients__swiper-bot', {
+  // Optional parameters
+  slidesPerView: 6,
+  spaceBetween: 30,
+});
+var mySwiper = new Swiper('.work-examples__swiper', {
+  // Optional parameters
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
+  slidesPerView: 3,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
