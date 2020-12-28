30e9bb38ffed1bf5de9972f7d901c91c6cb38bc9
Kate <catherina1punk@gmail.com>
2020-12-28 11:50:10 +0300

    + calc tabs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -19,6 +19,21 @@ if (document.querySelectorAll('.calc-table__cell--select')) {
   }
 }
 
+//calculator tabs
+for (const tabs of document.querySelectorAll("[data-tab]")) {
+  const buttons = tabs.querySelectorAll("[data-nav-tab]");
+  const items = tabs.querySelectorAll("[data-item-tab]");
+
+  for (const button of buttons) {
+    button.addEventListener("click", (event) => {
+      for (const item of items) {
+          item.classList.remove('active');
+      }
+          tabs.querySelector(`[data-item-tab='${event.target.dataset.navTab}']`).classList.add('active');
+      });
+  }
+}
+
 //header burger
 if (document.querySelector('.header__toggle')) {
   let btnToggle = document.querySelector('.header__toggle');
